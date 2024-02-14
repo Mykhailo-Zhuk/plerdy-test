083098102ff7b9709049392382adeff31a49e69a
Mykhailo-Zhuk <mishazhuk1@gmail.com>
2024-02-14 22:16:56 +0200

    some add validation

--- assets/index.js
+++ assets/index.js
@@ -51,16 +51,53 @@ $(document).ready(function () {
     $(window).scrollTop(0, 0);
   });
 
-  $("#layout").on("click", function () {
-    console.log("click");
-    $("#popup__container").fadeOut();
-    $("#layout").hide();
-    $("body").css("overflow-y", "auto");
+  // Close form when click on close button or ESC key
+  // Close form by clicking on the cross in the top right corner
+  $("#close__form, #layout").on("click", function () {
+    closeForm();
   });
 
-  $("#close_form").on("click", function () {
+  $(document).on("keydown", function (e) {
+    if (e.key === "Escape") {
+      closeForm();
+    }
+  });
+
+  function closeForm() {
     $("#popup__container").fadeOut();
     $("#layout").hide();
     $("body").css("overflow-y", "auto");
+  }
+
+  $(".form__body").submit(function (event) {
+    // Validate Name field
+    const nameValue = $("#name").val();
+
+    if (nameValue.trim() === "") {
+      $("#name").addClass("invalid");
+      $("#error__name").html("Name is required");
+      event.preventDefault();
+    } else {
+      $("#name").removeClass("invalid");
+      $("#error__name").html("");
+    }
+
+    // Validate Phone field
+    const phoneNumber = $("#phone").val();
+    validatePhoneNumber(phoneNumber);
+
+    function validatePhoneNumber(phoneNumber) {
+      const numericPhoneNumber = phoneNumber.replace(/\D/g, "");
+      const isValid = numericPhoneNumber.length === 9;
+
+      if (!isValid) {
+        $("#phone").addClass("invalid");
+        $("#error__phone").html("Please enter a valid 9-digit phone number.");
+        event.preventDefault();
+      } else {
+        $("#phone").removeClass("invalid");
+        $("#error__phone").html("");
+      }
+    }
   });
 });
